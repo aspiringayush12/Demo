204770755e700990739b3846197937ef43687dcf
aspiringayush12 <156356612+aspiringayush12@users.noreply.github.com>
2025-05-29 16:17:55 +0530

    third

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var path = require('path');  
+var app = express();
+
+var host = '127.0.0.1';
+var port = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); 
+
+app.get('/', function (req, res) {
+    res.render('index', { title: 'Welcome to EJS Page!' });
+});
+
+app.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
